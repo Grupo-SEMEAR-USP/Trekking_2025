2a0f7798336baa9e71679fc9c6449c891192c942
Giovanni <giovanni.santos.1811@usp.br>
2025-03-07 14:52:13 -0300

    Iniciada a transição do repositório 2024 para o de 2025

---
+++ Jetson/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/gola/Documents/SEMEAR/Trekking_2025/Jetson/catkin_ws/src
+# Build directory: /home/gola/Documents/SEMEAR/Trekking_2025/Jetson/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ Jetson/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/gola/Documents/SEMEAR/Trekking_2025/Jetson/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/gola/Documents/SEMEAR/Trekking_2024/Programação/RPi4/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/gola/Documents/SEMEAR/Trekking_2024/Programação/RPi4/catkin_ws/devel;/opt/ros/noetic' != '' else []
