d13945328887aa84f9f4a5002d2d733a8484c1af
Giovanni <giovanni.santos.1811@usp.br>
2025-05-21 11:00:46 -0300

    Adicionado launch básico da navigation stack (formação de local e global costmap)

--- Jetson/catkin_ws/src/navigation/scripts/tf_publisher.py
+++ Jetson/catkin_ws/src/navigation/scripts/tf_publisher.py
@@ -9,7 +9,7 @@ from tf.transformations import quaternion_from_euler
 class   TFClass:
     def __init__(self): #Constructor of the class
         self.global_frame_id = rospy.get_param('~global_frame_id', 'odom')
-        self.odom_publisher = rospy.Subscriber('/odom', Odometry, self.update_tf, queue_size=10)
+        #self.odom_publisher = rospy.Subscriber('/odom', Odometry, self.update_tf, queue_size=10)
 
         self.br = tf2_ros.TransformBroadcaster()
 
@@ -24,7 +24,7 @@ class   TFClass:
         self.t_map_odom.transform.translation.y = 0.0
         self.t_map_odom.transform.translation.z = 0.0
 
-        self.quartenion = quaternion_from_euler(0.0, 0.0, 1.570796) #orientação do frame map em relação ao odom
+        self.quartenion = quaternion_from_euler(0.0, 0.0, 0.0) #orientação do frame map em relação ao odom
 
         self.t_map_odom.transform.rotation.x = self.quartenion[0]
         self.t_map_odom.transform.rotation.y = self.quartenion[1]
@@ -32,7 +32,7 @@ class   TFClass:
         self.t_map_odom.transform.rotation.w = self.quartenion[3]
 
         #Defining the initial conditions of the transform between the odom and the base_link frames
-        self.t = geometry_msgs.msg.TransformStamped()
+        '''self.t = geometry_msgs.msg.TransformStamped()
         self.t.header.stamp = rospy.Time.now()
         self.t.header.frame_id = "/odom"
         self.t.child_frame_id = "base_link"
@@ -45,22 +45,22 @@ class   TFClass:
         self.t.transform.rotation.x = self.quartenion[0]
         self.t.transform.rotation.y = self.quartenion[1]
         self.t.transform.rotation.z = self.quartenion[2]
-        self.t.transform.rotation.w = self.quartenion[3]
+        self.t.transform.rotation.w = self.quartenion[3]'''
 
         self.sending_tfs()
 
 
     def update_time_stamps(self): #Updates the time stamps of all frames
         self.t_map_odom.header.stamp = rospy.Time.now()
-        self.t.header.stamp = rospy.Time.now()
+        #self.t.header.stamp = rospy.Time.now()
         
 
-    def update_tf(self, msg): #Callback function to the topic /odom
+    '''def update_tf(self, msg): #Callback function to the topic /odom
         #Filling the transform message with the information from odometry (tf between base_link and odom)
         self.t.transform.translation.x = msg.pose.pose.position.x
         self.t.transform.translation.y = msg.pose.pose.position.y
         self.t.transform.translation.z = 0.0
-        self.t.transform.rotation = msg.pose.pose.orientation
+        self.t.transform.rotation = msg.pose.pose.orientation'''
     
     def sending_tfs(self):
         while not rospy.is_shutdown():
@@ -68,7 +68,7 @@ class   TFClass:
 
             #Broadcasting the transforms of all frames
             self.br.sendTransform(self.t_map_odom) #Updating the transform between the map and the odom frames
-            self.br.sendTransform(self.t) #Updating the transform between odom and base_link
+            #self.br.sendTransform(self.t) #Updating the transform between odom and base_link
 
             self.rate.sleep()
 
